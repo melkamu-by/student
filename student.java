f1f7aa7488517bf66d2e9bd24ab718e869f104cc
melkamu-by <melkamuby23@gmail.com>
2026-02-16 10:28:26 +0300

    java

---
+++ student.java
@@ -0,0 +1,14 @@
+package student;
+class Student1 {
+
+}
+    public class student {
+        String Name;
+        int Id;
+    public static void main(String[] args) {
+        student a1 = new student();
+        System.out.println(a1.Name);
+        System.out.println(a1.Id);
+    }
+
+}
